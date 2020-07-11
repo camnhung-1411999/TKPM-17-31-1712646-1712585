249bb1df8956d99717938beda4458b23c3f079b2
camnhung-1411999 <55842532+camnhung-1411999@users.noreply.github.com>
2020-07-11 20:26:10 +0700

    create views for manager admin

---
+++ src/routes/admin.routes.ts
@@ -0,0 +1,31 @@
+import express = require('express');
+// import joi = require('joi');
+
+const router = express.Router();
+router.get(
+    '/',
+    (req,res)=>{
+        res.render('admin/home',{title:"admin"});
+    }
+)
+router.get(
+    '/category',
+    (req,res)=>{
+        res.render('admin/categories',{title:"Manage categories"});
+    }
+)
+router.get(
+    '/user',
+    (req,res)=>{
+        res.render('admin/user',{title:"Manager user"})
+    }
+)
+
+router.get(
+    '/order',
+    (req,res)=>{
+        res.render('admin/order',{title:"Manager order"})
+    }
+)
+
+export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ const router = express.Router();
 import userRoutes from './user.routes';
 import productRoutes from './product.routes';
 import authRoutes from './auth.routes';
+import adminRoutes from './admin.routes';
 router.get('/',(req,res)=>{
     res.render('home',{
         title: 'Home'
@@ -12,5 +13,6 @@ router.get('/',(req,res)=>{
 router.use('/users', userRoutes);
 router.use('/products',productRoutes);
 router.use('/auth',authRoutes);
+router.use('/admin',adminRoutes )
 
 export default router;
